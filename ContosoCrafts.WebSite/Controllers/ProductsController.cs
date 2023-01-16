d15e00e971ad026be1a4dd9a687b3419ade3bb6e
tharinda <tharindakodikara@outlook.com>
2023-01-16 12:51:39 +0530

    Add project files.

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService) 
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() 
+        { 
+            return ProductService.GetProducts();
+        }
+
+        // [HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRatings(ProductId, Rating);
+            return Ok();
+        }
+
+        [Route("Rate")]
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            ProductService.AddRatings(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
